1374d8aad564956100365cb29c4cc9d00da15447
proudsarmatian <thingsswamp@gmail.com>
2021-11-28 17:34:42 +0200

    practice5

--- src/main/java/ua/edu/sumdu/j2se/kravchenko/tasks/AbstractTaskList.java
+++ src/main/java/ua/edu/sumdu/j2se/kravchenko/tasks/AbstractTaskList.java
@@ -1,5 +1,7 @@
 package ua.edu.sumdu.j2se.kravchenko.tasks;
 
+import java.util.NoSuchElementException;
+
 public abstract class AbstractTaskList {
     protected int size;
 
@@ -8,8 +10,20 @@ public abstract class AbstractTaskList {
     public abstract boolean remove(Task task);
     public abstract Task getTask(int index);
 
+    public ListTypes.types listType (AbstractTaskList list) {
+        if (list instanceof ArrayTaskList) {
+            return ListTypes.types.ARRAY;
+        }
+        else if (list instanceof LinkedTaskList) {
+            return ListTypes.types.LINKED;
+        }
+        else {
+            throw new NoSuchElementException();
+        }
+    }
+
     public AbstractTaskList incoming(int from, int to){
-        AbstractTaskList newList = TaskListFactory.createTaskList(ListTypes.types.ARRAY);
+        AbstractTaskList newList = TaskListFactory.createTaskList(listType(this));
 
         for (int i = 0; i < this.size; ++i) {
             if (this.getTask(i).isActive() && this.getTask(i).getStartTime() > from &&
